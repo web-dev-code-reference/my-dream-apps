7bbfb93c232a16871f562c0c2aaef272870833ca
ialexies <ialexies@gmail.com>
2019-06-29 16:07:21 +0800

    test

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,14 +7,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int count=0;
+
   @override
   Widget build(BuildContext context) {
+ 
     return Scaffold(
       appBar: AppBar(
         title: new Text('My Dream Apps'),
         leading: Icon(Icons.ac_unit),
       ),
-      body: new Text('This is body'),
+      body:  dreamappsvew(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           addApp();
@@ -30,4 +34,25 @@ class _HomeState extends State<Home> {
       return AddApp();
     }));
   }
+
+
+ ListView dreamappsvew(){
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position){
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(backgroundColor: Colors.white, child: Icon(Icons.delete, color: Colors.grey,),),
+            						title: Text('Dummy title', ),
+						subtitle: Text('dummy Date'),
+						trailing: Icon(Icons.delete,color:Colors.grey,),
+						
+          ),
+        );
+      },
+    );
+  }
+
 }
